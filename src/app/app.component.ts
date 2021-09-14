ddef0c31ff2b9c9ee934894fddb739f9e6ab5312
artsanovich <artur.kartsan.silva@gmail.com>
2021-09-14 12:22:21 +0300

    add html, css template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string
-  text: string
-  id?: number
-}
 
 @Component({
   selector: 'app-root',
@@ -12,31 +7,12 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-posts: Post[] = [
-  {title: 'Want to learn Angular Components', text: 'I still learning components', id: 1},
-  {title: 'Next block', text: 'About direction', id: 2}
-]
+
+
 
   ngOnInit ():void {
-  setTimeout(() => {
-    console.log('Timeout')
-    this.posts[0] = {
-      title: 'Changed!',
-      text: 'Changed!\'Changed!\'',
-      id: 2
-    }
-  }, 5000)
   }
 
-  updatePosts(post: Post){
-  this.posts.unshift(post)
-
-  }
-
-  removePost(id: number) {
-    console.log('id to remove', id)
-    this.posts = this.posts.filter(p => p.id !== id)
-  }
 
 
 
