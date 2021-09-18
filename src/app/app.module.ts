a6376968077ae86063b798501b5a5e7acda7e6b2
artsanovich <artur.kartsan.silva@gmail.com>
2021-09-18 14:31:14 +0300

    добавили список задач

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import { CategoriesComponent } from './views/categories/categories.component';
+import { TasksComponent } from './views/tasks/tasks.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
