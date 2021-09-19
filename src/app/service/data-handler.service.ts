8f9f4915e5b380a31dc385224baeef26914462a4
artsanovich <artur.kartsan.silva@gmail.com>
2021-09-19 21:00:52 +0300

    rx tasks and categories

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,19 +2,26 @@ import { Injectable } from '@angular/core';
 import {Category} from "../model/Category";
 import {Task} from "../model/Task";
 import {TestData} from "../data/TestData";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
+  tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks)
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories)
+
   constructor() { }
 
-  getCategories(): Category[] {
-    return TestData.categories
+  fillTasks() {
+    this.tasksSubject.next(TestData.tasks)
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks
+  fillTasksByCategory(category: Category) {
+    const task = TestData.tasks.filter(task => task.category === category)
+    this.tasksSubject.next(task)
   }
+
+
 }
